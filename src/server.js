565f636f221bd298ef839526bf97ba0e1eda0e25
Alice <amyalicedee@gmail.com>
2016-07-06 15:15:29 +0100

    Add views

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static("views"));
+// app.use(express.bodyParser());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(multer());
+
 
 app.get("/", function(req, res) {
-  res.send("any string");
+  res.render("index.html");
 });
 
+app.post("/", function(req, res) {
+  res.send("meep");
+  // res.render("streaks.html", {
+  //   name: req.body.name
+  // });
+});
 module.exports = app;
